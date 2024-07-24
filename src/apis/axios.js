12b0d1d8d8d49a3654537fb817772f8b39fd14f4
veenjenga <vionajoynjenga@gmail.com>
2024-07-24 12:10:38 +0300

    changes

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,8 +1,8 @@
 // src/api/axios.js
-import axios from 'axios';
+import axios from "../apis/axios";
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', //  API base URL
+  baseURL: "http://127.0.0.1:8000", //  API base URL
   timeout: 1000, // Timeout in milliseconds
 });
 
